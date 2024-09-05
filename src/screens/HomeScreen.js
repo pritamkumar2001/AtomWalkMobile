37c715d40401e0272bcef77a6445d531c71876cd
Ashutosh Mohapatra <89243349+Ashutoshmo@users.noreply.github.com>
2024-09-05 16:32:17 +0530

    latest code

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,104 @@
-import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView, StatusBar, useWindowDimensions} from 'react-native'
+import { View, Text, StyleSheet, ImageBackground ,Image, SafeAreaView, ScrollView, StatusBar, useWindowDimensions,TouchableOpacity } from 'react-native'
 import React, {useState, useContext} from 'react'
 import { AuthContext } from '../context/AuthContext';
 import Header from '../components/Header';
 import CustomButton from '../components/CustomButton';
 import { colors } from '../Styles/appStyle'
-import Logo from '../../assets/images/Atom_walk_logo.jpg';
+import Logo from '../../assets/background.jpg'
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Logos from '../../assets/images/Atom_walk_logo.jpg';
+const AppContainer =  styled.View`
+  flex: 1;
+  background-color: #f5f5f5;
+
+`;
+
+const Headers = styled(ImageBackground)`
+  background-color: #0e4fe8;
+  padding: 20px;
+  padding-top: 60px;
+  padding-bottom: 105px;
+  align-items: center;
+  /* height: 40%; */
+`;
+
+const ProfileImage = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  border: 3px solid white;
+  margin-bottom: 10px;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const NotificationIcon = styled(Icon)`
+  position: absolute;
+  top: 60px;
+  right: 20px;
+  color: yellow;
+  font-size: 25px;
+`;
+
+const WelcomeText = styled.Text`
+  color: white;
+  font-size: 16px;
+  margin-top: 5px;
+`;
+const TbaView=styled.View`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+const TabContainer = styled.View`
+  background-color: white;
+  margin-top: -25px;
+  border-radius: 30px;
+  padding: 15px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  elevation: 5;
+  width: 95%;
+`;
+
+const TabItem = styled(TouchableOpacity)`
+  flex: 1;
+  align-items: center;
+  border-right: 1px solid black;
+`;
+
+const TabText = styled.Text`
+  color: #0e4fe8;
+  margin-top: 5px;
+
+`;
+
+const ListContainer = styled.View`
+  padding: 20px;
+  margin-top: 5%;
+`;
+
+const ListItem = styled(TouchableOpacity)`
+  background-color: white;
+  padding: 15px;
+  border-radius: 10px;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 15px;
+  elevation: 3;
+`;
+
+const ListText = styled.Text`
+  margin-left: 15px;
+  font-size: 16px;
+  color: #333;
+`;
 
 const data = [
   { label: 'Item 1', value: '1' },
@@ -26,64 +120,62 @@ const HomeScreen = ({ navigation }) => {
     setSelectedItem(item)
   }
   return (
-    <SafeAreaView style={{backgroundColor: colors.white, flex: 1}}>
-      <ScrollView contentContainerStyle={{paddingTop: 0, paddingHorizontal: 0}}>
-      <StatusBar barStyle="light-content" />
+  
       <>
-        <Header label={companyInfo?companyInfo.name:''}
-              image={companyInfo?companyInfo.image:''}
-              long={true}
-        />
-      
-        <View style={{backgroundColor: colors.white, alignItems: 'center', padding: 0, margin:10, height:200, borderRadius:6}}>
-          <Image 
-          source={Logo} 
-          style={{height: height* 0.3, maxHeight: 150, maxWidth: 200, width:'70%', }} 
-          resizeMode="contain"/>
-          <Text style={{color: colors.black, fontSize: 16, fontWeight: 'bold', paddingTop: 5}}>Wellcome to Atomwalk Office</Text>  
-        </View>
-
-        <View style={styles.container}>
-          
-          <CustomButton 
-          text='My Customers'
-          type='LARGE'
-          icon='contact-phone' 
-          onPress={() => navigation.navigate('CustomerScreen')} />
-
-          <CustomButton 
-          text='My Leads'
-          type='LARGE' 
-          icon='account-box'
-          onPress={() => navigation.navigate('LeadScreen')} />
-   
-
-          <CustomButton 
-          text='Invoice Status'
-          type='LARGE' 
-          icon='payments'
-          onPress={() => navigation.navigate('OrderList', {cust_id:'', cust_name:''})} />
-          
-          <CustomButton 
-          text='Company Info'
-          type='LARGE' 
-          icon='home-work'
-          onPress={() => navigation.navigate('Company')} />
-          
-        </View>
+        <StatusBar barStyle="light-content" backgroundColor="#0e4fe8" />
+      <AppContainer>
+       <Headers  source={Logo}>
+        <ProfileImage source={{ uri: companyInfo?companyInfo?.image:Logos}} />
+        <Title>{companyInfo?companyInfo.name:''}</Title>
+        <NotificationIcon name="notifications-outline" />
+        <WelcomeText>Welcome to Atomwalk Office</WelcomeText>
+      </Headers>
+      <TbaView>
+      <TabContainer>
+        <TabItem onPress={() => navigation.navigate('Task')}>
+          <Icon name="checkbox-outline" size={30} color="#0e4fe8" />
+          <TabText>Task</TabText>
+        </TabItem>
+        <TabItem onPress={() => navigation.navigate('Todo')}>
+          <Icon name="bulb-outline" size={30} color="#0e4fe8" />
+          <TabText>To Do</TabText>
+        </TabItem>
+        <TabItem onPress={() => navigation.navigate('Profile')}>
+          <Icon name="person-outline" size={30} color="#0e4fe8" />
+          <TabText>Profile</TabText>
+        </TabItem>
+      </TabContainer>
+      </TbaView>
+      <ListContainer>
+        <ListItem  onPress={() => navigation.navigate('CustomerScreen')}>
+          <Icon name="people-outline" size={30} color="#0e4fe8" />
+          <ListText>My Customers</ListText>
+        </ListItem>
+        <ListItem onPress={() => navigation.navigate('OrderList', {cust_id:'', cust_name:''})}>
+          <Icon name="file-tray-full-outline" size={30} color="#0e4fe8" />
+          <ListText>Invoice Status</ListText>
+        </ListItem>
+        <ListItem onPress={() => navigation.navigate('Company')}>
+          <Icon name="business-outline" size={30} color="#0e4fe8" />
+          <ListText>Company Info</ListText>
+        </ListItem>
+        <ListItem onPress={() => navigation.navigate('LeadScreen')}>
+          <Icon name="trending-up-outline" size={30} color="#0e4fe8" />
+          <ListText>My Leads</ListText>
+        </ListItem>
+      </ListContainer>
+    </AppContainer>
       </>
-    </ScrollView>          
-    </SafeAreaView>
-      
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:10,
-    padding: 10,
-    backgroundColor: colors.primary,
-    alignItems: 'center',
+    // borderRightColor:"#000",
+    // borderWidth: 1, // Border width of 2 pixels
+    // borderColor: '#000', // Border color blac
+    // borderStyle: 'solid', // Solid border style
+    // border
   },
 }) 
 
